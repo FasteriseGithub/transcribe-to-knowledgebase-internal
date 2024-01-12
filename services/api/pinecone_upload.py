323f9c185b6d992c72f2206d2199c95c3c9ba9ec
drat3 <drat3@protonmail.ch>
2024-01-12 21:51:27 +0100

    pinecone update

--- services/api/pinecone_upload.py
+++ services/api/pinecone_upload.py
@@ -1,22 +1,34 @@
 import pinecone
 import os
 from langchain_openai import OpenAIEmbeddings
+from langchain_community.vectorstores import Pinecone
 from types_internal import MetaData
 
-embeddings = OpenAIEmbeddings
+INDEX_NAME = "transcription-api"
+NAMESPACE_INTERNAL = "internal"
+NAMESPACE_DISCOVERY_CALLS = "discovery"
+
+embeddings = OpenAIEmbeddings()
 
 pinecone.init(
     api_key=os.getenv("PINECONE_API_KEY"),  # find at app.pinecone.io
     environment=os.getenv("PINECONE_ENV"),  # next to api key in console
 )
 
-index_name = "langchain-demo"
-
 async def embed_timed_transcript(transcript_chunked: list[str], metadata: MetaData):
-    pass
+    vectorstore = await create_index_if_not_exists(NAMESPACE_INTERNAL)
+    meta_dict = metadata.model_dump()
+
+    await vectorstore.aadd_texts(transcript_chunked, meta_dict)
 
 async def embed_summary(summary: str, metadata: MetaData):
-    pass
+    vectorstore = await create_index_if_not_exists(NAMESPACE_INTERNAL)
 
-async def create_index(index_name: str)
-    pass
+async def create_index_if_not_exists(namespace: str) -> Pinecone:
+    if INDEX_NAME not in pinecone.list_indexes():
+        pinecone.create_index(name=INDEX_NAME, metric="cosine", dimension=1536)
+
+    index = pinecone.Index(INDEX_NAME)
+    vectorstore = Pinecone(index, embeddings, namespace)
+
+    return vectorstore
--- services/api/types_internal.py
+++ services/api/types_internal.py
@@ -5,9 +5,10 @@ from enum import Enum
 from pydantic_core.core_schema import date_schema
 
 class MeetingTypeEnum(str, Enum):
-    marketing = "MARKETING"
-    general = "GEN"
-    development = "DEV"
+    marketing = "Marketing"
+    general = "General"
+    development = "Development"
+    outreach = "Outreach"
 
 class MetaData(BaseModel):
     date: date
